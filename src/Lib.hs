7f4bd105dc25ef2012e3d51d31d2b7a63882dee2
Mahler, Thomas <thomas.mahler@ista.com>
2020-01-22 10:55:43 +0100

    check in dojo 0

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib
     ( someFunc
     , squared
+    , manhattanDistance
+    , add
+    , Point (..)
     ) where
 
 someFunc :: IO ()
@@ -8,3 +11,11 @@ someFunc = putStrLn "someFunc"
 
 squared :: (Num a) => a -> a
 squared n = n*n
+
+add a b = a + b
+
+data Point = Point Int Int
+
+manhattanDistance :: (Point, Point) -> Int
+manhattanDistance (Point x y, Point x1 y1) = abs(x - x1) + abs(y -y1)
+
